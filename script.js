7bf18e5ad9a12beb5f5bc393ac99dfc4e013ecc2
David <d0pokrajac@gmail.com>
2024-11-18 09:21:39 +0100

    Added logic

---
+++ script.js
@@ -0,0 +1,78 @@
+let textArea = document.querySelector("textarea");
+let numbers = document.querySelectorAll(".number");
+let mathOperation = document.querySelectorAll(".math-operation");
+let equalsTo = document.querySelector(".equals");
+let result = "";
+let isCalculated = false;
+
+Array.from(numbers).forEach((number) => {
+    number.addEventListener("click", displayNumber);
+});
+
+Array.from(mathOperation).forEach((operation) => {
+    operation.addEventListener("click", displayOperator);
+});
+
+function displayNumber(event) {
+    const target = event.target;
+    if (
+        (target.textContent === "0" && textArea.textContent === "") ||
+        (target.textContent === "." && textArea.textContent === "")
+    ) {
+        return;
+    }
+    result += target.textContent;
+    textArea.textContent += target.textContent;
+}
+
+function displayOperator(event) {
+    const target = event.target;
+    if (textArea.textContent === "") {
+        return;
+    }
+    result += target.textContent;
+    textArea.textContent += target.textContent;
+    let hey = textArea.textContent;
+    for (let i = 0; i < textArea.textContent.length; i++) {
+        if (
+            textArea.textContent[i] === target.textContent &&
+            textArea.textContent[i - 1] === target.textContent
+        ) {
+            hey = hey.slice(0, textArea.textContent.length - 1);
+            result = hey;
+            textArea.textContent = hey;
+        }
+    }
+}
+
+equalsTo.addEventListener("click", () => {
+    checkLastValue(textArea.textContent[textArea.textContent.length - 1]);
+    textArea.textContent = eval(result);
+    isCalculated = true;
+});
+
+function reset() {
+    result = "";
+    textArea.textContent = "";
+    isCalculated = false;
+}
+
+function del() {
+    if (!result || isCalculated) {
+        return;
+    }
+    result = result.replace(result[result.length - 1], "");
+    textArea.textContent = result;
+}
+
+function checkLastValue(value) {
+    if (
+        value === "+" ||
+        value === "-" ||
+        value === "*" ||
+        value === "/" ||
+        value === "."
+    ) {
+        return;
+    }
+}
