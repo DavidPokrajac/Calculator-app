00d36630e28fe0614beff01da7d977be28c7d412
David <d0pokrajac@gmail.com>
2024-11-19 13:22:51 +0100

    Created styles for third theme

--- scriptTwo.js
+++ scriptTwo.js
@@ -21,5 +21,44 @@ function changeTheme(event) {
         calcEquals.className += " calculator__equals--theme-two";
         label.className += " label__--theme-two";
         ball.className += " ball__--theme-two";
+    } else if (event.target.value === "theme-3") {
+        document.body.className = document.body.className.replace(
+            "two",
+            "three"
+        );
+        calcDisplay.className = calcDisplay.className.replace("two", "three");
+        calcButtons.className = calcButtons.className.replace("two", "three");
+        calcDelete.className = calcDelete.className.replace("two", "three");
+        calcReset.className = calcReset.className.replace("two", "three");
+        calcEquals.className = calcEquals.className.replace("two", "three");
+        label.className = label.className.replace("two", "three");
+        ball.className = ball.className.replace("two", "three");
+    } else if (event.target.value === "theme-1") {
+        document.body.className = document.body.className.replace(
+            "body--theme-two",
+            ""
+        );
+        calcDisplay.className = calcDisplay.className.replace(
+            " calculator__display--theme-two",
+            ""
+        );
+        calcButtons.className = calcButtons.className.replace(
+            " calculator__buttons--theme-two",
+            ""
+        );
+        calcDelete.className = calcDelete.className.replace(
+            " calculator__delete--theme-two",
+            ""
+        );
+        calcReset.className = calcReset.className.replace(
+            " calculator__reset--theme-two",
+            ""
+        );
+        calcEquals.className = calcEquals.className.replace(
+            " calculator__equals--theme-two",
+            ""
+        );
+        label.className = label.className.replace(" label__--theme-two", "");
+        ball.className = ball.className.replace(" ball__--theme-two", "");
     }
 }
